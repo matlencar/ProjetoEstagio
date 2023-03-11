5e12e768da021110d55f3ac3b6199ef3211fcb75
matlencar <rm94642@fiap.com.br>
2023-03-11 13:09:46 -0300

    fix: concertando o erro da publicacao controller

--- src/main/java/br/com/fiap/projetoestagio/controllers/PublicacaoController.java
+++ src/main/java/br/com/fiap/projetoestagio/controllers/PublicacaoController.java
@@ -1,18 +1,20 @@
+package br.com.fiap.projetoestagio.controllers;
+
+
 import java.time.LocalDate;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.fiap.projetoestagio.models.Publicacao;
 
 @RestController
-public class PublicacaoController{
-    
+public class PublicacaoController {
+
     @GetMapping("/api/publicacao")
     @ResponseBody
-    public Publicacao show(){
+    public Publicacao show() {
 
         Publicacao publicacao = new Publicacao(new String("Ola"), LocalDate.now(), "Programando em java");
 
